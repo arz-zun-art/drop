139e5fb3a8a12026e0b1fd48496c688bb63ee23f
arz-zun-art <kt079551@gmail.com>
2026-03-23 16:02:45 +0545

    first js

--- node.js
+++ node.js
@@ -1,8 +1,8 @@
-//function collagefactuly() {
-//   console.log("bca,bct,civil,electronis");
-//}
-//collagefactuly();
-
-//var a = 10;
-//var b = 20;
-//console.log(a + b);//
+const http = require('http');
+const server = http.create server((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('arjun thakur');
+});
+server.listen(3000, () => {
+    console.log('Server running on http://localhost:3000');
+});
--- server1.js
+++ server1.js
@@ -1,7 +1,8 @@
+// Load Express
 const express = require('express');
 const app = express();
 
-// Middleware to parse JSON body
+// Middleware to parse JSON
 app.use(express.json());
 
 // Root route (GET)
@@ -9,20 +10,19 @@ app.get('/', (req, res) => {
     res.send('Hello! This is your HTTP server.');
 });
 
-// Another GET route
+// About route (GET)
 app.get('/about', (req, res) => {
     res.send('About page of your server.');
 });
 
-// POST route
+// POST route (receives data)
 app.post('/data', (req, res) => {
-    console.log(req.body); // logs incoming JSON
+    console.log(req.body); // log incoming data
     res.send({ message: 'Data received!', yourData: req.body });
 });
 
 // Start server
-app.listen(3000, () => {
-    console.log('Server running on http://localhost:3000');
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
 });
-
-// conslos
